89d88ff8fee1d8193cccc2fc57070fddb272d8d4
torikul570islam-cell <torikul570islam@gmail.com>
2026-09-14 01:51:15 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-app.set('trust proxy', 1); // রেট লিমিটারের প্রক্সি ইরর দূর করার জন্য
+app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cors());
 
